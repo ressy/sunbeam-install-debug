288bd2ca3c4f9c0e3b6acee066ac767967ba006f
Jesse Connell <ancon@upenn.edu>
2018-12-03 16:13:39 -0500

    more flexible setup wrapper

--- setup_wrapper.sh
+++ setup_wrapper.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-DIR=$(dirname $BASH_SOURCE)
+DIR=$(readlink -f $(dirname $BASH_SOURCE))
 # Remove existing conda, if any, and pre-pend this conda
 export PATH="$DIR/anaconda3/bin:$(echo $PATH | tr ':' '\n' | grep -v conda | tr '\n' ':')"
 # Run the given arguments
-"$@"
+if (( $# != 0 )); then
+	"$@"
+fi
