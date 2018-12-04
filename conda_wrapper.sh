72ef2f0aab256d5ef80ab9ccb3dbc01cd7a95e01
Jesse Connell <ancon@upenn.edu>
2018-12-04 10:17:42 -0500

    force HOME to working dir

--- conda_wrapper.sh
+++ conda_wrapper.sh
@@ -3,6 +3,12 @@
 DIR=$(readlink -f $(dirname $BASH_SOURCE))
 # Remove existing conda, if any, and pre-pend this conda
 export PATH="$DIR/anaconda3/bin:$(echo $PATH | tr ':' '\n' | grep -v conda | tr '\n' ':')"
+# No matter what I try (env variables, overriding files, etc.) conda still
+# wants to load things like channels from ~/.condarc.  This prevents conda from
+# looking elsewhere, though.
+export HOME="$DIR"
+export TMPDIR="$DIR/tmp"
+mkdir -p "$TMPDIR"
 # Run the given arguments
 if (( $# != 0 )); then
 	"$@"
